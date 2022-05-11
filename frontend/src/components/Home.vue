56f69b2127751fab4c1365597f5ef5ebf00b5e53
nrivoire <natacha.rivoire@yahoo.com>
2022-05-11 10:10:11 +0200

    initialisation frontend avec vuejs

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+// ./components/Home.vue
+<script>
+export default {
+	name: 'Home'
+}
+</script>
+
+<template>
+	<a href="/">Accueil</a> 
+</template>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
