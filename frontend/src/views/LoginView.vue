887397e8154d86a06f77101ad3ba1d8d56adef87
nrivoire <natacha.rivoire@yahoo.com>
2022-05-16 21:15:16 +0200

    gestion erreurs login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,4 +1,5 @@
 <template>
+
 	<body>
 		<main>
 			<div class="container">
@@ -9,18 +10,34 @@
 						<div class="imgcontainer">
 							<img src="../assets/icon-above-font.svg" height="300" alt="groupomania red logo">
 						</div>
-						<div class="container_login">
-							<div class="item">
-								<label for="email"><b>Email</b></label>
-								<input type="email" placeholder="Votre email" name="email" v-model="email" required>
+						<form>
+							<div class="container_login">
+								<div v-if="!this.errors.login" class="alert alert-dark" role="alert">
+									Wrong email or password
+								</div>
+								<div class="item">
+									<label for="email"><b>Email</b></label>
+									<input type="email" v-if="this.errors.email" placeholder="Votre email" name="email"
+										v-model="email" required>
+									<input type="email" v-else class="form-control is-invalid" placeholder="Votre email"
+										name="email" v-model="email" required>
+									<div id="validationServer04Feedback" class="invalid-feedback">
+										Il manque votre email
+									</div>
+								</div>
+								<div class="item">
+									<label for="password"><b>Mot de passe</b></label>
+									<input type="password" v-if="this.errors.password" placeholder="Votre mot de passe"
+										name="password" v-model="password" required>
+									<input type="password" v-else class="form-control is-invalid"
+										placeholder="Votre mot de passe" name="password" v-model="password" required>
+									<div id="validationServer04Feedback" class="invalid-feedback">
+										Il manque votre mot de passe
+									</div>
+								</div>
+								<button @click="loginSubmit" type="submit">Login</button>
 							</div>
-							<div class="item">
-								<label for="password"><b>Mot de passe</b></label>
-								<input type="password" placeholder="Votre mot de passe" name="password"
-									v-model="password" required>
-							</div>
-							<button @click="loginSubmit" type="submit">Login</button>
-						</div>
+						</form>
 					</div>
 					<div class="col">
 					</div>
@@ -37,29 +54,51 @@ export default {
 		return {
 			email: '',
 			password: '',
+			errors: {
+				email: 1,
+				password: 1,
+				login: 1
+			}
 		}
 	},
 	methods: {
-		loginSubmit() {
-			fetch("http://localhost:3000/api/login", {
-				method: "POST",
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					email: this.email,
-					password: this.password
+		loginSubmit(e) {
+			e.preventDefault();
+			if (this.email && this.password) {
+				this.errors.email = 1;
+				this.errors.password = 1;
+				fetch("http://localhost:3000/api/login", {
+					method: "POST",
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						email: this.email,
+						password: this.password
+					})
+				}).then(res => {
+					if (res.ok) {
+						this.errors.login = 1;
+						return res.json();
+					} else
+						this.errors.login = 0;
+				}).then(data => {
+					if (data) {
+						sessionStorage.setItem("Token", JSON.stringify(data.token));
+						sessionStorage.setItem("UserId", JSON.stringify(data.userId));
+						window.location = "/session";
+					}
+				}).catch(err => {
+					console.error(err);
 				})
-			}).then(res => {
-				return res.json();
-			}).then(data => {
-				sessionStorage.setItem("Token", JSON.stringify(data.token));
-				sessionStorage.setItem("UserId", JSON.stringify(data.userId));
-				window.location = "/session";
-			}).catch(err => {
-				console.error(err);
-			})
+			}
+			if (!this.email) {
+				this.errors.email = 0;
+			}
+			if (!this.password) {
+				this.errors.password = 0;
+			}
 		}
 	}
 }
